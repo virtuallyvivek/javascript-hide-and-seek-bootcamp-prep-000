57f2e42eb76687344a0f1f54d759a8458816b938
Catherine Straus <vnair611@gmail.com>
2020-07-05 02:38:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,8 +11,7 @@ function increaseRankBy(n) {
   const lis = document.querySelectorAll('ul.ranked-list li');
  
   for (let i = 0; i < lis.length; i++) {
-   var newValue = parseInt(lis[i].innerHTML) + n
-   lis[i].innerHTML = newValue
+   lis[i].innerHTML = parseInt(lis[i].innerHTML) + n
   }
 }
 
